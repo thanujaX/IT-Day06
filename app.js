8c2863344ec6504ad81da74403fabb1aff62381c
thanujaX <thanujahasaranga1@gmail.com>
2026-07-04 15:29:19 +0530

    Implement customer management functionality and update HTML structure in index.html

--- app.js
+++ app.js
@@ -44,19 +44,60 @@
 // console.log(customer2.items[2].name);
 // console.log(customer2.father.name);
 
-console.log(document.title);
+// console.log(document.title);
 
-let heading = document.getElementById("heading");
-console.log(heading);
+// let heading = document.getElementById("heading");
+// console.log(heading);
 
-heading.innerText = "THANUJA";
+// heading.innerText = "THANUJA";
 
-let number = 0;
-function changeHeading() {
-    heading.innerText = "Thanuja" + (++number);
-    console.log("clicked!");
-}
-function changeHeading2() {
-    heading.innerText = "Thanuja" + (--number);
-    console.log("clicked!");
+// let number = 0;
+// function changeHeading() {
+//     heading.innerText = "Thanuja" + (++number);
+//     console.log("clicked!");
+// }
+// function changeHeading2() {
+//     heading.innerText = "Thanuja" + (--number);
+//     console.log("clicked!");
+// }
+
+
+// function changeHeading() {
+
+//     let txtInput = document.getElementById("inputText");
+
+//     let userInput = txtInput.value; 
+
+//     console.log(userInput);
+    
+//     let heading = document.getElementById("heading");
+
+//     heading.innerText=userInput;
+// }
+// let number = 0;
+// function changeHeading() {
+//     heading.innerText = "" + (console.log(userInput));
+//     console.log("clicked!");
+// }
+
+let customerList = [];
+
+function btnAddcustomerOnAction() {
+    let txtName = document.getElementById("txtName").value;
+    let txtAddress = document.getElementById("txtAddress").value;
+    let txtAge = document.getElementById("txtAge").value;
+    let txtEmail = document.getElementById("txtEmail").value;
+    let txtSalary = document.getElementById("txtSalary").value;
+
+    let customer = {
+        name: txtName,
+        age: txtAge,
+        address: txtAddress,
+        email: txtEmail,
+        salary: txtSalary
+    }
+
+    customerList.push(customer);
+
+    console.log(customerList);
 }
