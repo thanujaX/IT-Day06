be62bf59151e768ea821b0ad493c2ab4def76656
thanujaX <thanujahasaranga1@gmail.com>
2026-07-04 11:14:57 +0530

    Add customer2 object and console logs to app.js; update index.html to include app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+// let customer2 = {
+//     name: "John",
+//     age: 25,
+//     address: "New York",
+//     salary: 50000,
+//     items: [
+//         {
+//             id: 1,
+//             name: "item1",
+//             price: 100,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name: "variant1",
+//                     price: 10
+//                 },
+//                 {
+//                     id: 2,
+//                     name: "variant2",
+//                     price: 20
+                    
+//                 }
+//             ]
+//         }, {
+//             id: 2,
+//             name: "item2",
+//             price: 200
+//         }, {
+//             id: 3,
+//             name: "item3",
+//             price: 300
+//         }
+//     ],
+//     father: {
+//         name: "Doe",
+//         age: 50,
+//         address: "New York"
+//     }
+// };
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.salary);
+// console.log(customer2.items[2].name);
+// console.log(customer2.father.name);
+
+console.log(document.title);
+
+let heading = document.getElementById("heading");
+console.log(heading);
